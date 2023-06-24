8079843e9567c83498bec83120887438eabc734b
aayyus <91897477+aayyus@users.noreply.github.com>
2023-06-24 20:01:30 +0545

    task

---
+++ .next/server/font-loader-manifest.js
@@ -0,0 +1,9 @@
+self.__FONT_LOADER_MANIFEST={
+  "pages": {
+    "/_app": [
+      "static/media/84b8ad28acc25855.p.woff2",
+      "static/media/83c76cede88902c5.p.woff2"
+    ]
+  },
+  "app": {}
+}
